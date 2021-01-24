99af931f00cc69484556b05dd30fb7cd98d468d5
Denis Gorobets <d.gorobets@drweb.com>
2021-01-24 12:42:19 +0300

    use erlang cookie file

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -2,22 +2,26 @@
 
 set -e
 
-# Start RMQ from entry point.
-# This will ensure that environment variables passed
-# will be honored
-/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+rabbitmq-server -detached
 
-# Do the cluster dance
+# Wait for start server
+sleep 3
+
 rabbitmqctl stop_app
 rabbitmqctl join_cluster rabbit@rabbitmq1
+rabbitmqctl start_app
+
+if [ "$HOSTNAME" == rabbitmq2 ]; then
+    rabbitmqctl add_user ${RABBITMQ_USER} ${RABBITMQ_PASS}
+    rabbitmqctl set_user_tags ${RABBITMQ_USER} administrator
+    rabbitmqctl set_permissions -p / ${RABBITMQ_USER} ".*" ".*" ".*"
+    rabbitmqctl delete_user guest
+fi
 
 # Stop the entire RMQ server. This is done so that we
 # can attach to it again, but without the -detached flag
 # making it run in the forground
 rabbitmqctl stop
-
-# Wait a while for the app to really stop
-sleep 2s
 
 # Start it
 rabbitmq-server
